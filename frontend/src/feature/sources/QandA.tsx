a599c8b1afe8790e4be6d6c75a3a89f52f40b196
John Hashim <johnhashim10@gmail.com>
2025-12-13 22:49:04 +0530

    feat: ui starting for q&a and text source upload page

--- frontend/src/feature/sources/QandA.tsx
+++ frontend/src/feature/sources/QandA.tsx
@@ -1,8 +1,11 @@
 export const UploadQandA: React.FC = () => {
   return (
-    <div>
-      <header className="px-5 py-2 flex items-center justify-between">
-        <p>File</p>
+    <div className="px-4">
+      <header className="text-center sm:text-left">
+        <p className="text-2xl font-semibold">Q&A</p>
+        <p className="text-sm mt-1 font-light text-text-weak max-w-2/3 mx-auto sm:mx-0">
+          Teach your AI how to respond to frequently asked questions
+        </p>
       </header>
     </div>
   )
--- frontend/src/feature/sources/Text.tsx
+++ frontend/src/feature/sources/Text.tsx
@@ -1,8 +1,13 @@
+import '@mantine/tiptap/styles.css'
+
 export const UploadText: React.FC = () => {
   return (
-    <div>
-      <header className="px-5 py-2 flex items-center justify-between">
-        <p>File</p>
+    <div className="px-4">
+      <header className="text-center sm:text-left">
+        <p className="text-2xl font-semibold">Text</p>
+        <p className="text-sm mt-1 font-light text-text-weak max-w-2/3 mx-auto sm:mx-0">
+          Add text to train your AI
+        </p>
       </header>
     </div>
   )
